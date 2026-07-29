7d5672548e2bf9bd10ecd8ef6b16b158f189f896
Programmer Rakibul <rakibul00206@gmail.com>
2026-07-29 17:04:18 +0600

    refactor: simplify RootLayout by removing conditional rendering of Navbar and Footer; reorganize imports in router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,14 +1,14 @@
 import DashboardLayout from "@/layouts/dashboard-layout";
 import RootLayout from "@/layouts/root-layout";
-import SignInPage from "@/pages/sign-in-page";
-import SignUpPage from "@/pages/sign-up-page";
+import AddTaskPage from "@/pages/add-task-page";
 import AdminAllTasksPage from "@/pages/admin-all-tasks-page";
 import AdminAllUsersPage from "@/pages/admin-all-users-page";
 import DashboardOverview from "@/pages/dashboard-overview";
-import ProfilePage from "@/pages/profile-page";
 import HomePage from "@/pages/home-page";
-import AddTaskPage from "@/pages/add-task-page";
 import MyTasksPage from "@/pages/my-tasks-page";
+import ProfilePage from "@/pages/profile-page";
+import SignInPage from "@/pages/sign-in-page";
+import SignUpPage from "@/pages/sign-up-page";
 import { Role } from "@/types/user";
 import { createBrowserRouter } from "react-router";
 import ProtectedRoute from "./protected-route";
@@ -18,59 +18,32 @@ export const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "sign-in",
-        element: <SignInPage />,
-      },
-      {
-        path: "sign-up",
-        element: <SignUpPage />,
-      },
+      { index: true, element: <HomePage /> },
+      { path: "sign-in", element: <SignInPage /> },
+      { path: "sign-up", element: <SignUpPage /> },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
       {
         element: <ProtectedRoute />,
         children: [
+          { index: true, element: <DashboardOverview /> },
+          { path: "profile", element: <ProfilePage /> },
           {
-            path: "dashboard",
-            element: <DashboardLayout />,
+            element: <ProtectedRoute allowedRoles={[Role.USER]} />,
             children: [
-              {
-                index: true,
-                element: <DashboardOverview />,
-              },
-              {
-                path: "profile",
-                element: <ProfilePage />,
-              },
-              {
-                element: <ProtectedRoute allowedRoles={[Role.USER]} />,
-                children: [
-                  {
-                    path: "add-task",
-                    element: <AddTaskPage />,
-                  },
-                  {
-                    path: "my-tasks",
-                    element: <MyTasksPage />,
-                  },
-                ],
-              },
-              {
-                element: <ProtectedRoute allowedRoles={[Role.ADMIN]} />,
-                children: [
-                  {
-                    path: "all-tasks",
-                    element: <AdminAllTasksPage />,
-                  },
-                  {
-                    path: "all-users",
-                    element: <AdminAllUsersPage />,
-                  },
-                ],
-              },
+              { path: "add-task", element: <AddTaskPage /> },
+              { path: "my-tasks", element: <MyTasksPage /> },
+            ],
+          },
+          {
+            element: <ProtectedRoute allowedRoles={[Role.ADMIN]} />,
+            children: [
+              { path: "all-tasks", element: <AdminAllTasksPage /> },
+              { path: "all-users", element: <AdminAllUsersPage /> },
             ],
           },
         ],
